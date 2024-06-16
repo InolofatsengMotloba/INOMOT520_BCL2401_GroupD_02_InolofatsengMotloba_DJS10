a1b171aa39a84aab67cd6c19d681db659a01da55
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-16 11:25:34 +0200

    Add comments to BlogPosts.jsx

--- src/BlogPosts.jsx
+++ src/BlogPosts.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 const BlogPosts = () => {
-  const [posts, setPosts] = useState([]);
-  const [error, setError] = useState(null);
+  const [posts, setPosts] = useState([]); // State to hold posts fetched from API
+  const [error, setError] = useState(null); // State to handle errors during fetching
 
+  // useEffect hook to fetch data
   React.useEffect(() => {
     async function fetchPosts() {
       try {
+        // Fetch data from JSONPlaceholder API
         const response = await fetch(
           "https://jsonplaceholder.typicode.com/posts"
         );
-        
 
+        // Check if response is unsuccessful
         if (!response.ok) {
           throw new Error("Data fetching failed");
         }
@@ -20,17 +22,21 @@ const BlogPosts = () => {
         const data = await response.json();
         setPosts(data);
       } catch (error) {
+        // Catch and handle any errors during fetch
         setError(error.message);
       }
     }
 
+    // Call fetchPosts function
     fetchPosts();
   }, []);
 
+  // Render UI based on unsucessful state
   if (error) {
     return <h1>Data fetching failed</h1>;
   }
 
+  // Render posts if fetched successfully
   return (
     <div>
       <h1>Posts</h1>
