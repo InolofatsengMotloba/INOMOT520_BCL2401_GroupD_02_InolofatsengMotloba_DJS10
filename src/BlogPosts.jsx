495cc12cc106ac0fffff40159af3d3265e2639ee
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-16 11:18:45 +0200

    Adjust code to fit instructions

--- src/BlogPosts.jsx
+++ src/BlogPosts.jsx
@@ -11,12 +11,13 @@ const BlogPosts = () => {
         const response = await fetch(
           "https://jsonplaceholder.typicode.com/posts"
         );
-        const data = await response.json();
+        
 
         if (!response.ok) {
           throw new Error("Data fetching failed");
         }
 
+        const data = await response.json();
         setPosts(data);
       } catch (error) {
         setError(error.message);
