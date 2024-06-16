67aebe14d9f2630dfce04bad1a6a1120cdcece9c
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-16 10:47:53 +0200

    API Integration: Add useEffect to fetch posts from API and handle errors

---
+++ src/BlogPosts.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+export default BlogPosts = () => {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+
+  React.useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        const data = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Data fetching failed");
+        }
+
+        setPosts(data);
+      } catch (error) {
+        setError(error.message);
+      }
+    }
+
+    fetchPosts();
+  }, []);
+
+
+  return <></>;
+};
